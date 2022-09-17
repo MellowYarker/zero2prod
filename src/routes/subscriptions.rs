2f84e1e68161a9fee0123179ba577710f2966010
Milan Miladinovic <milanmiladinovic@outlook.com>
2022-09-17 14:42:07 -0400

    Insert to DB on subscribe.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, HttpResponse};
-use std::net::TcpListener;
+use sqlx::PgPool;
+use chrono::Utc;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -7,6 +9,32 @@ pub struct FormData {
     name: String
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+// web::Data is an extractor, but what's it extracting a PgConnection from?
+// When a new request comes in, actix computes the TypeId of the type specified in the signature,
+// checks if there is a record corresponding to it in the type-map (actix handles this), and if
+// there is it casts the retrieved Any value to the specified type.
+//
+// Other languages refer to this as a dependency injection.
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to the `PgConnection`
+    // wrapped by `web::Data`.
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,26 @@
-use actix_web::{web, App, HttpServer};
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-use crate::routes::*;
 
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword.
 // We have no .await call, so it is not needed anymore!
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // We wrap the pool in an Arc since it must be cloneable.
+    // Why must it be cloneable? Well HttpServer::new() expects the closure to return
+    // an instance of an App. Why? So that each thread (worker) can have one.
+    //
+    // Actually, we can wrap it in web::Data because it's really just a wrapper for an Arc.
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
-            .route("/health_check", web::get().to(health_check::health_check))
-            .route("/subscriptions", web::post().to(subscriptions::subscribe))
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            // Register the connection as part of the application state.
+            .app_data(db_pool.clone())
         })
         .listen(listener)?
         .run();
