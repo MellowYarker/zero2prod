3217f157d406824bcc55066d2af84a1667ce7922
Milan Miladinovic <milanmiladinovic@outlook.com>
2023-01-30 22:04:56 -0500

    Chapter 6 - Rejecting Invalid Subscribers
    This chapter focused on "Parse, don't validate", and making sure our
    tests were solid. We covered property-based testing, and making sure we
    enforce correctness through types (focusing on local code rather than
    global).

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,13 +1,26 @@
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
+use std::convert::{TryFrom, TryInto};
 use chrono::Utc;
 use uuid::Uuid;
 use tracing::Instrument;
+use unicode_segmentation::UnicodeSegmentation;
+use crate::domain::{NewSubscriber, SubscriberName, SubscriberEmail};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
     name: String
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
 }
 
 #[tracing::instrument(
@@ -19,15 +32,24 @@ pub struct FormData {
     )
 )]
 
-// web::Data is an extractor, but what's it extracting a PgConnection from?
+// web::Data is an extractor, but what is it extracting a PgConnection from?
 // When a new request comes in, actix computes the TypeId of the type specified in the signature,
 // checks if there is a record corresponding to it in the type-map (actix handles this), and if
 // there is it casts the retrieved Any value to the specified type.
 //
 // Other languages refer to this as a dependency injection.
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await
-    {
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>
+) -> HttpResponse {
+
+    //  `web::Form` is a wrapper around `FormData` `form.0` gives us access to the underlying
+    //  `FormData`
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish()
     }
@@ -35,11 +57,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
 pub async fn insert_subscriber(
     pool: &PgPool,
-    form: &FormData,
+    new_subscriber: &NewSubscriber,
+    // form: &FormData,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
@@ -47,8 +70,8 @@ pub async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     // We use `get_ref` to get an immutable reference to the `PgConnection`
